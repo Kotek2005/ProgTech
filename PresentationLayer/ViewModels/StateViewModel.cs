b68d89a16e7169892f6ba87410b2be4099001779
Kotek2005 <151841030+Kotek2005@users.noreply.github.com>
2025-05-19 19:16:08 +0200

    Perhaps Perchance

--- PresentationLayer/ViewModels/StateViewModel.cs
+++ PresentationLayer/ViewModels/StateViewModel.cs
@@ -2,22 +2,32 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 using DataLayer;
+using LogicLayer;
 
 namespace PresentationLayer.ViewModels
 {
     public class StateViewModel : INotifyPropertyChanged
     {
         private readonly IEvents _events;
+        private readonly Buy_class _buyer;
+        private readonly Supply_class _supplier;
         private ObservableCollection<StateModel> _inventory;
         private float _currentCash;
         private string _newProductName;
         private int _newProductAmount;
+        private string _selectedProduct;
+        private int _buyAmount;
+        private int _supplyAmount;
 
         public StateViewModel(IEvents events)
         {
             _events = events;
+            _buyer = new Buy_class(events);
+            _supplier = new Supply_class(events);
             Inventory = new ObservableCollection<StateModel>();
             AddStockCommand = new RelayCommand(AddStock, CanAddStock);
+            BuyCommand = new RelayCommand(Buy, CanBuy);
+            SupplyCommand = new RelayCommand(Supply, CanSupply);
             RefreshState();
         }
 
@@ -63,7 +73,42 @@ namespace PresentationLayer.ViewModels
             }
         }
 
+        public string SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                _selectedProduct = value;
+                OnPropertyChanged(nameof(SelectedProduct));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        public int BuyAmount
+        {
+            get => _buyAmount;
+            set
+            {
+                _buyAmount = value;
+                OnPropertyChanged(nameof(BuyAmount));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        public int SupplyAmount
+        {
+            get => _supplyAmount;
+            set
+            {
+                _supplyAmount = value;
+                OnPropertyChanged(nameof(SupplyAmount));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
         public ICommand AddStockCommand { get; }
+        public ICommand BuyCommand { get; }
+        public ICommand SupplyCommand { get; }
 
         private void AddStock()
         {
@@ -78,10 +123,37 @@ namespace PresentationLayer.ViewModels
             return !string.IsNullOrWhiteSpace(NewProductName) && NewProductAmount > 0;
         }
 
+        private void Buy()
+        {
+            if (_buyer.Buy(SelectedProduct, BuyAmount))
+            {
+                RefreshState();
+                BuyAmount = 0;
+            }
+        }
+
+        private bool CanBuy()
+        {
+            return !string.IsNullOrWhiteSpace(SelectedProduct) && BuyAmount > 0;
+        }
+
+        private void Supply()
+        {
+            if (_supplier.Supply(SelectedProduct, SupplyAmount))
+            {
+                RefreshState();
+                SupplyAmount = 0;
+            }
+        }
+
+        private bool CanSupply()
+        {
+            return !string.IsNullOrWhiteSpace(SelectedProduct) && SupplyAmount > 0;
+        }
+
         private void RefreshState()
         {
             Inventory.Clear();
-            // Get inventory from Events_class
             var inventory = _events.GetAllInventory();
             foreach (var item in inventory)
             {
