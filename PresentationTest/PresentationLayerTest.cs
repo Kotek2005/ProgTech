91c0b60fbab3b2a4c4f8b5f8767095ea52fcc09e
Kotek2005 <151841030+Kotek2005@users.noreply.github.com>
2025-05-20 13:05:25 +0200

    PresTestStructure

---
+++ PresentationTest/PresentationLayerTest.cs
@@ -0,0 +1,11 @@
+﻿namespace PresentationTest
+{
+    [TestClass]
+    public sealed class PresentationLayerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
